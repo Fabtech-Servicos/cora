56ae5d8830f093d8c54dbca8c549137c836c102b
PauloHenrique2004 <51000157+PauloHenrique2004@users.noreply.github.com>
2020-12-21 10:57:16 -0300

    update InstagramController.php

--- src/Controller/InstagramController.php
+++ src/Controller/InstagramController.php
@@ -3,12 +3,20 @@ namespace App\Controller;
 
 use Cake\Controller\Controller;
 
+use InstagramScraper\Instagram;
+use Phpfastcache\Helper\Psr16Adapter;
+
 class InstagramController extends Controller
 {
     public function instagram()
     {
         $this->autoRender = false;
         $this->response = $this->response->withType('json');
+
+        $instagram  = Instagram::withCredentials(new \GuzzleHttp\Client(), 'he_nrique2811', '@q1w2e3', new Psr16Adapter('Files'));
+        $instagram->login();
+        $instagram->saveSession();
+
         $this->response = $this->response->withStringBody(
             file_get_contents("https://www.instagram.com/fabtech_/?__a=1")
         );
